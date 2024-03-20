accf2437c1bcd8b2ed423483ada407fe3d65c075
Artem Chernyshov <achernyshovv1@gmail.com>
2024-03-20 15:15:35 +0200

    Added models for ingredients, dish types, cooks, and dishes

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Ingredients"
+
+    def __str__(self):
+        return self.name
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["first_name"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=5, decimal_places=2, validators=[MaxValueValidator(1000)]
+    )
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    class Meta:
+        ordering = ["-price"]
+
+    def __str__(self):
+        return f"{self.name}: ${self.price}"
